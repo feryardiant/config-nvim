ad7c23bda18293e2b991adbba47732e8b76a7160
Fery Wardiyanto <ferywardiyanto@gmail.com>
2025-01-22 17:49:07 +0700

    chore: messing around with editor colors and add missing editorconfig file
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -116,16 +116,36 @@ return {
       { 'nvim-tree/nvim-web-devicons' },
     },
     keys = {
-      { '<leader>xx', '<Cmd>Trouble diagnostics toggle<CR>', desc = '[Trouble] Diagnostics' },
-      { '<leader>xX', '<Cmd>Trouble diagnostics toggle filter.buf=0<CR>', desc = '[Trouble] Buffer diagnostics' },
-      { '<leader>xs', '<Cmd>Trouble symbols toggle focus=false<CR>', desc = '[Trouble] Symbols' },
+      {
+        '<leader>xx',
+        '<Cmd>Trouble diagnostics toggle<CR>',
+        desc = '[Trouble] Diagnostics'
+      },
+      {
+        '<leader>xX',
+        '<Cmd>Trouble diagnostics toggle filter.buf=0<CR>',
+        desc = '[Trouble] Buffer diagnostics'
+      },
+      {
+        '<leader>xs',
+        '<Cmd>Trouble symbols toggle focus=false<CR>',
+        desc = '[Trouble] Symbols'
+      },
       {
         '<leader>xl',
         '<Cmd>Trouble lsp toggle focus=false win.position=right<CR>',
         desc = '[Trouble] LSP Definitions',
       },
-      { '<leader>xL', '<Cmd>Trouble loclist toggle<CR>', desc = '[Trouble] Location list' },
-      { '<leader>xq', '<Cmd>Trouble qflist toggle<CR>', desc = '[Trouble] Quickfix list' },
+      {
+        '<leader>xL',
+        '<Cmd>Trouble loclist toggle<CR>',
+        desc = '[Trouble] Location list'
+      },
+      {
+        '<leader>xq',
+        '<Cmd>Trouble qflist toggle<CR>',
+        desc = '[Trouble] Quickfix list'
+      },
     },
     opts = {
       use_diagnostic_signs = true,
