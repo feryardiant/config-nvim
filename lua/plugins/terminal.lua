1e4d66268999a3b40afe42176bbffa2cebc32d10
Fery Wardiyanto <ferywardiyanto@gmail.com>
2025-02-04 10:45:06 +0700

    chore(terminal): swap `autocmd` with `on_open` opts
    just to make the configuration cleaner
    
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- lua/plugins/terminal.lua
+++ lua/plugins/terminal.lua
@@ -10,32 +10,25 @@ return {
       open_mapping = '<c-`>',
       float_opts = {
         border = 'curved',
-      }
+      },
+
+      ---@param term Terminal
+      on_open = function (term)
+        vim.opt_local.signcolumn = 'no'
+
+        if string.find(term.name, vim.o.shell) then
+          vim.keymap.set('t', '<Esc>', '<C-\\><C-n>', { buffer = term.bufnr, desc = 'Swich to normal mode' })
+
+          -- vim.keymap.set('t', 'jk', '<C-\><C-n>', opts)
+          -- vim.keymap.set('t', '<C-w>', '<C-\\><C-n><C-w>', opts)
+          vim.keymap.set('t', '<C-h>', '<Cmd>wincmd h<CR>', { buffer = term.bufnr, desc = 'Go to left window'  })
+          vim.keymap.set('t', '<C-j>', '<Cmd>wincmd j<CR>', { buffer = term.bufnr, desc = 'Go to window below' })
+          vim.keymap.set('t', '<C-k>', '<Cmd>wincmd k<CR>', { buffer = term.bufnr, desc = 'Go to window above' })
+          vim.keymap.set('t', '<C-l>', '<Cmd>wincmd l<CR>', { buffer = term.bufnr, desc = 'Go to right window' })
+        end
+      end
     },
     init = function ()
-      -- Configure terminal's local options and keymaps
-      vim.api.nvim_create_autocmd({ 'TermOpen' }, {
-        pattern = 'term://*',
-        callback = function (args)
-          local opts = { buffer = 0 }
-          local file_match = args.match or args.file
-
-          -- Disable sign-column in terminal window
-          vim.opt_local.signcolumn = 'no'
-
-          if not string.find(file_match, 'lazygit') then
-            vim.keymap.set('t', '<Esc>', '<C-\\><C-n>', opts)
-
-            -- vim.keymap.set('t', 'jk', '<C-\><C-n>', opts)
-            -- vim.keymap.set('t', '<C-w>', '<C-\\><C-n><C-w>', opts)
-            vim.keymap.set('t', '<C-h>', '<Cmd>wincmd h<CR>', { desc = 'Go to left window'  })
-            vim.keymap.set('t', '<C-j>', '<Cmd>wincmd j<CR>', { desc = 'Go to window below' })
-            vim.keymap.set('t', '<C-k>', '<Cmd>wincmd k<CR>', { desc = 'Go to window above' })
-            vim.keymap.set('t', '<C-l>', '<Cmd>wincmd l<CR>', { desc = 'Go to right window' })
-          end
-        end
-      })
-
       local Terminal = require('toggleterm.terminal').Terminal
 
       local lazygit = Terminal:new({
