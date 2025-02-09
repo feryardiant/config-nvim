8040bd27798f101450e8d27280ea61d05954ed4f
Fery Wardiyanto <ferywardiyanto@gmail.com>
2025-02-09 19:26:37 +0700

    feat(lint): ditch `eslint_d` in regards of `eslint`
    see #5
    
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -7,19 +7,7 @@ return {
 
       lint.linters_by_ft = {
         markdown = { 'markdownlint' },
-        javascript = { 'eslint_d' },
-        typescript = { 'eslint_d' },
-        javascriptreact = { 'eslint_d' },
-        typescriptreact = { 'eslint_d' },
-        svlete = { 'eslint_d' },
-        vue = { 'eslint_d' },
       }
-
-      lint.linters.eslint_d = require('lint.util').wrap(lint.linters.eslint_d, function(diagnostic)
-        if diagnostic.message:find('Error: Could not find config file') then return nil end
-
-        return diagnostic
-      end)
 
       vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
         group = vim.api.nvim_create_augroup('lint', { clear = true }),
