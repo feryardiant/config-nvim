45ec65070ffc841079618d92fa7efca937e93d03
Fery Wardiyanto <ferywardiyanto@gmail.com>
2025-02-11 23:41:39 +0700

    feat(statusline): messing around with `lualine` configs

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -178,19 +178,72 @@ return {
     'nvim-lualine/lualine.nvim',
     dependencies = {
       { 'nvim-tree/nvim-web-devicons' },
+      { 'folke/trouble.nvim' },
     },
     opts = function(_, opts)
       vim.o.laststatus = vim.g.lualine_laststatus
 
       opts.options = {
-        theme = 'auto',
         globalstatus = true,
         disabled_filetypes = { 'dashboard', 'alpha', 'starter' },
-        componnet_separators = '',
-        section_separators = '',
+        component_separators = '',
       }
 
-      opts.extensions = { 'neo-tree', 'lazy', 'toggleterm' }
+      opts.extensions = {
+        'lazy',
+        'mason',
+        'neo-tree',
+        'nvim-dap-ui',
+        'quickfix',
+        'toggleterm',
+        'trouble',
+      }
+
+      local filetype = {
+        'filetype',
+        icon_only = true,
+        padding = { left = 1, right = 0 },
+      }
+
+      local filename = {
+        'filename',
+        padding = { left = 0, right = 1 },
+        path = 1,
+        symbols = {
+          modified = '',
+          readonly = '󰗖',
+          unnamed = '',
+          newfile = '󱨧'
+        },
+      }
+
+      local doc_symbols = require('trouble').statusline({
+        mode = 'lsp_document_symbols',
+        groups = {},
+        max_items = 4,
+        title = false,
+        filter = { range = true },
+        format = '{kind_icon}{symbol.name:Comment}',
+        hl_group = 'lualine_c_normal',
+      })
+
+      local symbols = {
+        doc_symbols.get,
+        cond = doc_symbols.has,
+        padding = { left = 1, right = 0 },
+      }
+
+      opts.sections = {
+        lualine_a = { 'mode' },
+        lualine_b = {
+          { 'branch', icon = { '', align = 'right' } },
+          { 'diff', padding = { left = 0, right = 1 } },
+        },
+        lualine_c = { filetype, filename, symbols },
+        lualine_x = { 'diagnostics', 'fileformat' },
+        lualine_y = { 'progress' },
+        lualine_z = { 'location' },
+      }
     end,
     init = function()
       vim.g.lualine_laststatus = vim.o.laststatus
