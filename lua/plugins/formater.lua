bd97625063cf44f36fb2c5dbd6f72dcabe0df41d
Fery Wardiyanto <ferywardiyanto@gmail.com>
2025-02-12 17:39:54 +0700

    refactor(formater): rearrange `conform` configs

--- lua/plugins/formater.lua
+++ lua/plugins/formater.lua
@@ -16,40 +16,40 @@ return {
         desc = '[F]ormat [B]uffer',
       },
     },
-    opts = function()
+    ---@module 'conform'
+    ---@param opts conform.setupOpts
+    opts = function(_, opts)
       local util = require('conform.util')
 
-      ---@module 'conform'
-      ---@type conform.setupOpts
-      return {
-        log_level = vim.log.levels.WARN,
-        formatters = {
-          prettier = {
-            prepend_args = { '--ignore-unknown' },
-          },
-          pint = {
-            meta = {
-              url = 'https://github.com/laravel/pint',
-              description = 'Laravel Pint is an opinionated PHP code style fixer for minimalists.',
-            },
-            command = util.find_executable({
-              vim.fn.stdpath('data') .. '/mason/bin/pint',
-              'vendor/bin/pint',
-            }, 'pint'),
-            args = { '$FILENAME' },
-            stdin = false,
-          },
+      opts.log_level = vim.log.levels.WARN
+
+      opts.formatters = {
+        prettier = {
+          prepend_args = { '--ignore-unknown' },
         },
-        formatters_by_ft = {
-          blade = { 'prettier', 'blade-formatter', stop_after_first = true },
-          json = { 'prettier', 'jq', stop_after_first = true },
-          javascript = { 'prettier', stop_after_first = true },
-          lua = { 'stylua' },
-          markdown = { 'prettier', 'markdownlint', stop_after_first = true },
-          php = { 'pint', stop_after_first = true },
-          sql = { 'pg_format', 'sqlfmt', stop_after_first = true },
-          yaml = { 'yamlfmt' },
+        pint = {
+          meta = {
+            url = 'https://github.com/laravel/pint',
+            description = 'Laravel Pint is an opinionated PHP code style fixer for minimalists.',
+          },
+          command = util.find_executable({
+            vim.fn.stdpath('data') .. '/mason/bin/pint',
+            'vendor/bin/pint',
+          }, 'pint'),
+          args = { '$FILENAME' },
+          stdin = false,
         },
+      }
+
+      opts.formatters_by_ft = {
+        blade = { 'prettier', 'blade-formatter', stop_after_first = true },
+        json = { 'prettier', 'jq', stop_after_first = true },
+        javascript = { 'prettier', stop_after_first = true },
+        lua = { 'stylua' },
+        markdown = { 'prettier', 'markdownlint', stop_after_first = true },
+        php = { 'pint', stop_after_first = true },
+        sql = { 'pg_format', 'sqlfmt', stop_after_first = true },
+        yaml = { 'yamlfmt' },
       }
     end,
     init = function() vim.o.formatexpr = "v:lua.require('conform').formatexpr()" end,
