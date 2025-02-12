a1db2908a1b4eeee3f702f54e948ec09b7149c61
Fery Wardiyanto <ferywardiyanto@gmail.com>
2025-02-12 20:27:53 +0700

    refactor(editor): move `notification` config to `editor`
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -16,6 +16,77 @@ return {
   },
 
   {
+    'rcarriga/nvim-notify',
+    lazy = true,
+    ---@module 'notify'
+    ---@type notify.Config
+    opts = {
+      minimum_width = 25,
+      -- max_width = 50,
+      background_colour = '#000000',
+    },
+  },
+
+  {
+    'folke/noice.nvim',
+    event = 'VeryLazy',
+    dependencies = {
+      { 'MunifTanjim/nui.nvim' },
+      { 'rcarriga/nvim-notify' },
+    },
+    ---@module 'noice'
+    ---@param opts NoiceConfig
+    opts = function (_, opts)
+      opts.lsp = {
+        override = {
+          ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+          ['vim.lsp.util.stylize_markdown'] = true,
+          ['cmp.entry.get_documentation'] = false,
+        },
+        -- signature = {
+        --   enabled = false,
+        -- },
+        -- hover = {
+        --   enabled = false
+        -- }
+      }
+
+      opts.messages = {
+        view = 'mini',
+      }
+
+      opts.commands = {
+        -- Show all notification history
+        ---@see https://github.com/craftzdog/dotfiles-public/blob/a445967/.config/nvim/lua/plugins/ui.lua#L35-L40
+        all = {
+          view = 'split',
+          opts = { enter = true, format = 'details', border = 'none' },
+          filter = { event = 'notify' },
+        }
+      }
+
+      opts.presets = {
+        bottom_search = true, -- use a classic bottom cmdline for search
+        command_palette = true, -- position the cmdline and popupmenu together
+        long_message_to_split = true, -- long messages will be sent to a split
+        -- inc_rename = false, -- enables an input dialog for inc-rename.nvim
+        lsp_doc_border = true -- add a border to hover docs and signature help
+      }
+
+      opts.routes = {
+        -- Credit: https://github.com/neovim/nvim-lspconfig/issues/1931#issuecomment-2138428768
+        {
+          filter = {
+            event = 'notify',
+            find = 'No information available',
+          },
+          opts = { skip = true },
+        },
+      }
+    end
+  },
+
+  {
     'lukas-reineke/indent-blankline.nvim',
     event = { 'BufReadPre', 'BufNewFile' },
     main = 'ibl',
