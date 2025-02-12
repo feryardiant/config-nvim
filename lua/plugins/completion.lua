97338c886d3936641c5d966d2898dbafe15d6f6a
Fery Wardiyanto <ferywardiyanto@gmail.com>
2025-02-12 16:29:00 +0700

    chore(completion): trigger `nvim-cmp` in `InsertEnter`

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -1,7 +1,7 @@
 return {
   {
     'hrsh7th/nvim-cmp',
-    event = { 'BufReadPre', 'BufNewFile' },
+    event = { 'InsertEnter' },
     dependencies = {
       { 'hrsh7th/cmp-buffer' },
       { 'hrsh7th/cmp-nvim-lsp' },
