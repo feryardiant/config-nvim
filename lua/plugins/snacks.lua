1147e5b6c57c856e37ba375aae75928023bdbfeb
Fery Wardiyanto <ferywardiyanto@gmail.com>
2025-03-02 02:59:23 +0700

    chore(snacks): replace `neo-tree` with `snacks.explorer`

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -6,20 +6,36 @@ return {
     keys = {
       {
         '<leader>n',
-        function()
-          Snacks.picker.notifications()
-        end,
-        desc = 'Notification history'
+        function() Snacks.picker.notifications() end,
+        desc = 'Show [N]otifications',
+      },
+      {
+        '<leader>fe',
+        function() Snacks.picker.explorer() end,
+        desc = 'Open [F]ile [E]xplorer',
       },
     },
     ---@module 'snacks'
     ---@type snacks.Config
     opts = {
+      explorer = {
+        replace_netrw = true,
+      },
       input = {
         enabled = true,
       },
       notifier = {
         enabled = true,
+      },
+      picker = {
+        layouts = {
+          sidebar = {
+            layout = {
+              position = 'right',
+              width = 32,
+            },
+          },
+        },
       },
     },
   },
