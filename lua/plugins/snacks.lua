a5b99e35107e8df1bd0856c8c827176f0bee2672
Fery Wardiyanto <ferywardiyanto@gmail.com>
2025-03-02 09:43:35 +0700

    chore(snacks): messing around with keymapping

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -8,10 +8,13 @@ return {
       { '<C-p>', function() Snacks.picker.projects() end, desc = '[P]rojects' },
       { '<leader>n', function() Snacks.picker.notifications() end, desc = 'Show [N]otifications' },
       { '<leader><space>', function() Snacks.picker.files() end, desc = 'Find files' },
-      { '<leader>fe', function() Snacks.explorer({ follow_file = true }) end, desc = 'Open [F]ile [E]xplorer' },
+      { '<leader>f', function() Snacks.explorer({ follow_file = true }) end, desc = '[F]ile Explorer' },
       { '<leader>ff', function() Snacks.picker.buffers() end, desc = 'Find Bu[ff]ers' },
       { '<leader>fg', function() Snacks.picker.git_files() end, desc = '[F]ind Current [G]it Repo' },
-      { '<leader>rf', function() Snacks.rename.rename_file() end, desc = '[R]ename File' },
+      { '<leader>r', function() Snacks.rename.rename_file() end, desc = '[R]ename File' },
+      -- Diagnostics
+      { '<leader>d', function() Snacks.picker.diagnostics() end, desc = 'Open [D]iagnostics' },
+      { '<leader>da', function() Snacks.picker.diagnostics_buffer() end, desc = 'Open [D]iagnostics Buffer' },
       -- Searches
       { '<leader>sw', function() Snacks.picker.grep_word() end, desc = '[S]earch by [W]ord', mode = { "n", "x" } },
       { '<C-f>', function() Snacks.picker.grep() end, desc = 'Live grep' },
@@ -24,8 +27,8 @@ return {
       { 'gr', function() Snacks.picker.lsp_references() end, desc = '[G]oto [R]eferences', nowait = true },
       { 'gI', function() Snacks.picker.lsp_implementations() end, desc = '[G]oto [I]mplementation' },
       { 'gy', function() Snacks.picker.lsp_type_definitions() end, desc = '[G]oto T[y]pe Definition' },
-      { '<leader>ss', function() Snacks.picker.lsp_symbols() end, desc = '[S]earch Document [S]ymbols' },
-      { '<leader>sS', function() Snacks.picker.lsp_workspace_symbols() end, desc = '[S]earch Workspace [S]ymbols' },
+      { '<leader>s', function() Snacks.picker.lsp_workspace_symbols() end, desc = 'Workspace Symbols' },
+      { '<leader>ss', function() Snacks.picker.lsp_symbols() end, desc = 'Document Symbols' },
     },
     ---@module 'snacks'
     ---@type snacks.Config
