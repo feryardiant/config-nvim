7c78987d6db1931ff9e477def0aeaef2333fb6dc
Fery Wardiyanto <ferywardiyanto@gmail.com>
2025-03-22 21:27:33 +0700

    feat(lsp): add `markdownlint` json and yaml schema

--- lua/settings.lua
+++ lua/settings.lua
@@ -84,6 +84,10 @@ return {
         url = 'https://json.schemastore.org/package',
       },
       {
+        fileMatch = { '.markdownlint.json', '.markdownlint.jsonc' },
+        url = 'https://raw.githubusercontent.com/DavidAnson/markdownlint/main/schema/markdownlint-config-schema.json',
+      },
+      {
         fileMatch = { '.prettierrc.json', '.prettierrc' },
         url = 'https://json.schemastore.org/prettierrc.json',
       },
@@ -99,6 +103,7 @@ return {
       ['https://gitlab.com/gitlab-org/gitlab/-/raw/master/app/assets/javascripts/editor/schema/ci.json'] = '.gitlab-ci.yml',
       ['http://json.schemastore.org/composer'] = 'composer.yaml',
       ['https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json'] = 'docker-compose*.yml',
+      ['https://raw.githubusercontent.com/DavidAnson/markdownlint/main/schema/markdownlint-config-schema.json'] = '.markdownlint.{yml,yaml}',
       ['https://raw.githubusercontent.com/kamilkisiela/graphql-config/v3.0.3/config-schema.json'] = '.graphqlrc*',
       ['https://json.schemastore.org/github-workflow.json'] = '.github/workflow/*.yml',
     },
