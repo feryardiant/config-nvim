9d26936678e8419234644b86b8429ca9d867870f
Fery Wardiyanto <ferywardiyanto@gmail.com>
2025-03-28 09:41:53 +0700

    chore: init `conform` :fire:
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,54 @@
+return {
+  {
+    'stevearc/conform.nvim',
+    event = { 'BufWritePre' },
+    cmd = { 'ConformInfo' },
+    keys = {
+      {
+        '<leader>fo',
+        function()
+          require('conform').format({
+            async = true,
+            lsp_format = 'fallback',
+            timeout_ms = 5000,
+          })
+        end,
+        desc = '[Fo]rmat Buffer',
+      },
+    },
+    ---@module 'conform'
+    ---@param opts conform.setupOpts
+    opts = function(_, opts)
+      local util = require('conform.util')
+
+      opts.log_level = vim.log.levels.WARN
+
+      opts.formatters = {
+        prettier = {
+          prepend_args = { '--ignore-unknown' },
+        },
+        pint = {
+          meta = {
+            url = 'https://github.com/laravel/pint',
+            description = 'Laravel Pint is an opinionated PHP code style fixer for minimalists.',
+          },
+          command = util.find_executable({
+            vim.fn.stdpath('data') .. '/mason/bin/pint',
+            'vendor/bin/pint',
+          }, 'pint'),
+          args = { '$FILENAME' },
+          stdin = false,
+        },
+      }
+
+      opts.formatters_by_ft = {
+        lua = { 'stylua' },
+        php = { 'pint', stop_after_first = true },
+      }
+    end,
+    init = function()
+      -- stylua: ignore
+      vim.o.formatexpr = "v:lua.require('conform').formatexpr()"
+    end,
+  },
+}
