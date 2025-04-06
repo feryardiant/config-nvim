cdc198f1baa4f13788314f8496899fc69318f6eb
Fery Wardiyanto <ferywardiyanto@gmail.com>
2025-04-07 04:54:20 +0700

    fix(git): snacks lazygit won't open on unstaged files

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,38 +1,43 @@
+local keymap = require('utils.keymap')
+local map = keymap.create()
+
+map('n', '<leader>gg', function() Snacks.lazygit() end, { desc = 'Open LazyGit' })
+map('n', '<leader>gl', function() Snacks.picker.git_log({ layout = 'default' }) end, { desc = 'Open [G]it [L]og' })
+
 return {
   {
     'lewis6991/gitsigns.nvim',
     event = { 'BufReadPre', 'BufNewFile' },
     ---@module 'gitsigns'
-    ---@type Gitsigns.Config
-    opts = {
-      current_line_blame = true,
-      signs = {
+    ---@param opts Gitsigns.Config
+    opts = function (_, opts)
+      opts.current_line_blame = true
+
+      opts.signs = {
         add = { text = '▎' },
         change = { text = '▎' },
         changedelete = { text = '▎' },
         untracked = { text = '▎' },
-      },
-      on_attach = function(bufnr)
-        ---@type gitsigns.actions
+      }
+
+      opts.on_attach = function (buffer)
+        ---@type Gitsigns.actions
         local gs = package.loaded.gitsigns
-        local map = require('utils.keymap').create({ buffer = bufnr })
+        local bmap = keymap.create({ buffer = buffer })
 
-        map('n', '<leader>gg', function() Snacks.lazygit() end, { desc = 'Open LazyGit' })
-        map('n', '<leader>gl', function() Snacks.picker.git_log({ layout = 'default' }) end, { desc = 'Open [G]it [L]og' })
+        bmap('n', ']h', function() gs.nav_hunk('next') end, { desc = 'Next [H]unk' })
+        bmap('n', '[h', function() gs.nav_hunk('prev') end, { desc = 'Prev [H]unk' })
+        bmap('n', ']H', function() gs.nav_hunk('last') end, { desc = 'Last [H]unk' })
+        bmap('n', '[H', function() gs.nav_hunk('first') end, { desc = 'First [H]unk' })
 
-        map('n', ']h', function() gs.nav_hunk('next') end, { desc = 'Next [H]unk' })
-        map('n', '[h', function() gs.nav_hunk('prev') end, { desc = 'Prev [H]unk' })
-        map('n', ']H', function() gs.nav_hunk('last') end, { desc = 'Last [H]unk' })
-        map('n', '[H', function() gs.nav_hunk('first') end, { desc = 'First [H]unk' })
+        bmap({ 'n', 'v' }, '<leader>hr', gs.reset_hunk, { desc = 'Reset [H]unk' })
+        bmap({ 'n', 'v' }, '<leader>hs', gs.stage_hunk, { desc = 'Stage [H]unk' })
+        bmap('n', '<leader>hS', gs.stage_buffer, { desc = 'Stage Buffer' })
+        bmap('n', '<leader>hR', gs.reset_buffer, { desc = 'Reset Buffer' })
 
-        map({ 'n', 'v' }, '<leader>hr', gs.reset_hunk, { desc = 'Reset [H]unk' })
-        map({ 'n', 'v' }, '<leader>hs', gs.stage_hunk, { desc = 'Stage [H]unk' })
-        map('n', '<leader>hS', gs.stage_buffer, { desc = 'Stage Buffer' })
-        map('n', '<leader>hR', gs.reset_buffer, { desc = 'Reset Buffer' })
-
-        map('n', '<leader>h?', gs.preview_hunk_inline, { desc = 'Preview [H]unk' })
-      end,
-    },
+        bmap('n', '<leader>h?', gs.preview_hunk_inline, { desc = 'Preview [H]unk' })
+      end
+    end
   },
 
   {
