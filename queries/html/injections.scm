0c5c951e93f27b3178aa41539ccf343f07356c98
Fery Wardiyanto <ferywardiyanto@gmail.com>
2026-09-08 20:02:53 +0700

    refactor(queries): remove `blade` custom query since it already comes built-in from `treesitter-blade`
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- queries/html/injections.scm
+++ queries/html/injections.scm
@@ -1,35 +1,18 @@
-;; extends
+; extends: html
 
 ; AlpineJS attributes
+; <div x-data="{ foo: 'bar' }" x-init="baz()">
 (attribute
   (attribute_name) @_attr
-    (#lua-match? @_attr "^x%-%l")
+    (#match? @_attr "^x-[a-z]+")
+    (#not-any-of? @_attr "x-teleport" "x-ref" "x-transition")
   (quoted_attribute_value
     (attribute_value) @injection.content)
   (#set! injection.language "javascript"))
-
-; Blade escaped JS attributes
-; <x-foo ::bar="baz" />
-(element
-  (_
-    (tag_name) @_tag
-      (#lua-match? @_tag "^x%-%l")
-  (attribute
-    (attribute_name) @_attr
-      (#lua-match? @_attr "^::%l")
-    (quoted_attribute_value
-      (attribute_value) @injection.content)
-    (#set! injection.language "javascript"))))
-
-; Blade PHP attributes
-; <x-foo :bar="$baz" />
-(element
-  (_
-    (tag_name) @_tag
-      (#lua-match? @_tag "^x%-%l")
-    (attribute
-      (attribute_name) @_attr
-        (#lua-match? @_attr "^:%l")
-      (quoted_attribute_value
-        (attribute_value) @injection.content)
-      (#set! injection.language "php_only"))))
+; <div :foo="bar" @click="baz()">
+(attribute
+  (attribute_name) @_attr
+    (#match? @_attr "^[:@][a-z]+")
+  (quoted_attribute_value
+    (attribute_value) @injection.content)
+  (#set! injection.language "javascript"))
